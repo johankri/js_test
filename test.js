6a7a92db543f1eca8fb8e9220c862f40fdff2930
Johan Kristiansson <johankristi@gmail.com>
2023-04-15 20:55:37 +0200

    Test

--- test.js
+++ test.js
@@ -1,27 +1,58 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
-async function fetchCards() {
-    // JK: query
-    // JK: Multiple pages
-    const url = 'https://api.scryfall.com/cards/search?q=%28set%3Amom+or+set%3Amul%29+%28r%3Ac+or+r%3Au%29';
-    const response = await fetch(url);
-    const newVar = await response.json();
-    return newVar.data;
-}
-
-function getCard(sheet, distribution) {
-    const random = Math.random();
-    let sum = 0;
-    return distribution.find(x => {
-        sum = sum + Object.values(x)[0];
+function getRarity(distribution) {
+    const random = Math.random()
+    let sum = 0
+    return Object.keys(distribution).find(key => {
+        sum = sum + distribution[key]
         return random < sum
     });
 }
 
-const sheet = null;
-const distribution = [{"m": (1 / 5) * (1 / 3)}, {"r": (4 / 5) * (1 / 3)}, {"u": 2 / 3}];
-const foo = getCard(sheet, distribution)
-console.log(foo)
+async function getRandomCard(query) {
+    const url = `https://api.scryfall.com/cards/random?q=${encodeURIComponent(query + ' in:booster')}`
+    const response = await fetch(url)
+    return await response.json()
+}
+
+async function getSpecialSheetCard(query) {
+    const distribution = {"m": (1 / 5) * (1 / 3), "r": (4 / 5) * (1 / 3), "u": 2 / 3}
+    const rarity = getRarity(distribution)
+    return await getRandomCard(`${query} and r:${rarity}`)
+}
+
+async function getRare(query) {
+    const distribution = {"m": (1 / 7.4) * (1 / 3), "r": (6.4 / 7.4)}
+    const rarity = getRarity(distribution)
+    return await getRandomCard(`${query} and r:${rarity}`)
+}
+
+async function getUncommon(query) {
+    return await getRandomCard(`${query} and r:u`)
+}
+
+async function getCommon(query) {
+    return await getRandomCard(`${query} and r:c`)
+}
+
+async function getLand(query) {
+    return undefined;
+}
+
+let cards = []
+cards.push(await getSpecialSheetCard("s:MUL"))
+cards.push(await getSpecialSheetCard("s:MOM t:Battle"))
+cards.push(await getSpecialSheetCard("s:MOM is:doublefaced -t:battle"))
+cards.push(await getRare("s:MOM")) // JK: Uncommon
+for (let i = 0; i < 2; i++) {
+    cards.push(await getUncommon("s:MOM"))
+}
+for (let i = 0; i < 8; i++) {
+    cards.push(await getCommon("s:MOM"))
+}
+cards.push(await getLand("s:MOM"))
+console.log(cards)
+
 
 // Mystical Archive: Uncommon 2/3, Rare 26.4, mythic rare 6.6
 
